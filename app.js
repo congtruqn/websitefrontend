bc362cc70eb49dbe5e3f4d982a322b983a752bbc
Tran Cong Tru <trutc@innotech-vn.com>
2022-09-19 13:14:02 +0700

    1

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 const limiter = rateLimit({
 	windowMs: 60 * 1000, // 1 minutes
-	max: 700, // Limit each IP to 2000 requests per `window` (here, per 15 minutes)
+	max: 200, // Limit each IP to 2000 requests per `window` (here, per 1 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
