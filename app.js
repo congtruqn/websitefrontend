8eceae392e076ab07a477daebfeb3d6b38276542
Tru Tran <congtruqn@gmail.com>
2023-12-15 21:43:11 +0700

    111

--- app.js
+++ app.js
@@ -172,8 +172,7 @@ app.use(function (req, res, next) {
 });
 app.use('/api', api);
 app.use('/', index);
-
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
@@ -225,18 +224,15 @@ else {
   };
   mongoose.connect("mongodb://" + process.env.DB_URL + "/website", options);
 }
-
 app.use(async function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next();
-});// error handler
+});
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   console.log(err);
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
