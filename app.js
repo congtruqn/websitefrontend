b04682ebbfe959b2099345922ba3fd3a123d8f30
Tru Tran <congtruqn@gmail.com>
2025-01-19 22:59:09 +0700

    update

--- app.js
+++ app.js
@@ -177,40 +177,15 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
-if (process.env.NODE_ENV && process.env.NODE_ENV == 'local') {
-  const tunnelOptions = {
-    autoClose: true
+
+if (process.env.NODE_ENV && process.env.NODE_ENV == 'develop') {
+  const options = {
+    user: process.env.MONGO_USER,
+    pass: process.env.MONGO_PASSWORD,
+    keepAlive: true,
+    keepAliveInitialDelay: 300000,
+    useNewUrlParser: true
   };
-  const serverOptions = {
-    port: 27017
-  };
-  const sshOptions = {
-    host: process.env.DB_URL,
-    port: 22,
-    username: 'root',
-    password: process.env.SSH_PASS
-  };
-  const forwardOptions = {
-    srcAddr: '0.0.0.0',
-    srcPort: 27017,
-    dstAddr: '127.0.0.1',
-    dstPort: 27017
-  };
-  createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions).then(([server, conn], error) => {
-    server.on('error', (e) => {
-      console.log(e);
-    });
-    conn.on('error', (e) => {
-      console.log(e);
-    });
-    conn.on('success', (e) => {
-      console.log(e);
-    });
-    mongoose.connect(`mongodb://localhost/${process.env.WEBSITE_DB_NAME}`);
-  });
-  ;
-}
-else if(process.env.NODE_ENV && process.env.NODE_ENV == 'develop'){
   mongoose.connect("mongodb://" + process.env.DB_URL + "/website_dev");
 }
 else {
